97a3a2774698a85e592b67d3ccc691e088b87fdc
azharf99 <azharfaturohman29@gmail.com>
2026-04-11 12:44:12 +0700

    Refactor import statement in App component to simplify React usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { type JSX } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { getUserRole } from './utils/auth';
 
